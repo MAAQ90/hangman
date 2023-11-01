320856ffe38e3c8e430c7f72d64c2327909d41e0
MAAQ90 <134720024+MAAQ90@users.noreply.github.com>
2023-11-01 22:10:07 +0000

    Update to if-else block
    Updates milestone_2 to if-else block

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,12 @@
 import random
 
 word_list = ['mango', 'orange', 'banana', 'apple', 'melon']
-print(type(word_list), ": ", word_list)
+#print(type(word_list), ": ", word_list)
 
 word = random.choice(word_list)
-print(word)
+#print(word)
 
 guess = input('Please enter a single letter: ')
 
-while (guess.isalpha() != True) or (len(guess) != 1):
-    guess = input('Oops! That is not a valid input. Please retry: ')
-else:
-    print('\nGood guess!')
+print('\nGood guess!') if len(guess) == 1 else print('\nOops! That is not a valid input.')
     
